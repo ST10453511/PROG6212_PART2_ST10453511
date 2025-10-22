41488acbb38f528059515f4e70d0ee6554b88671
Moegammad Yaaseen Alexander <ST10453511@imconnect.edu.za>
2025-10-22 22:07:22 +0200

    Added my Controller files that contains all my working code

--- PROG6212_POE/Controllers/HomeController.cs
+++ PROG6212_POE/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
-using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using PROG6212_POE.Models;
+using System.Diagnostics;
 
 namespace PROG6212_POE.Controllers
 {
@@ -13,17 +14,42 @@ namespace PROG6212_POE.Controllers
             _logger = logger;
         }
 
+        /// <summary>
+        /// Public welcome/landing page. If the user is already authenticated,
+        /// we skip this page and send them to the dashboard.
+        /// </summary>
+        [HttpGet]
+        [AllowAnonymous]
         public IActionResult Index()
+        {
+            if (User?.Identity?.IsAuthenticated ?? false)
+                return RedirectToAction("Index", "Dashboard");
+
+            return View();
+        }
+
+        /// <summary>
+        /// Optional public about page describing the system.
+        /// </summary>
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult About()
         {
             return View();
         }
 
+        /// <summary>
+        /// Keep your existing Privacy page public.
+        /// </summary>
+        [HttpGet]
+        [AllowAnonymous]
         public IActionResult Privacy()
         {
             return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        [AllowAnonymous]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
