cd653ff861ee0b49b78801e114537334ab9bee2c
Moegammad Yaaseen Alexander <ST10453511@imconnect.edu.za>
2025-10-22 23:08:52 +0200

    Updated my Approval messages for the coordinators

--- PROG6212_POE/obj/Debug/net9.0/PROG6212_POE.AssemblyInfo.cs
+++ PROG6212_POE/obj/Debug/net9.0/PROG6212_POE.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PROG6212_POE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e193b73ffe1442164e96ec9fa6503a2c373cbc63")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4df9912999676678447ac03ab657335ab0f0030a")]
 [assembly: System.Reflection.AssemblyProductAttribute("PROG6212_POE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PROG6212_POE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
